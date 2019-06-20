b2c7832d31f1fbc4a13e8bd9618caea179cbd98a
Ahmed Saad <ahmed.saad200253@gmail.com>
2019-06-20 06:10:51 +0200

    Login - SignUp

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'loginScreen.dart';
+void main() {
+  runApp(MaterialApp(
+    title: "STEM Guide",
+    home: LoginScreen(),
+    routes: {
+//      'home':(context) => home(),
+    },
+  ));
+}

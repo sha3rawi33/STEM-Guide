e808ded60dcd425156f7472d1597ba804bc8f4b8
Ahmed Saad <ahmed.saad200253@gmail.com>
2019-06-20 09:44:51 +0200

    Image constants

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,16 +1,3 @@
-import 'package:flutter/painting.dart';
-import 'package:flutter/material.dart';
-
-
-class AppColors {
-  static const Color darkGrey = const Color(0xFF666666);
-  static const Color bodyText = const Color(0xFF9d9d9d);
-  static const Color boldText = const Color(0xFF222222);
-  static const Color lightGrey = const Color(0xFFd1d1d1);
-  static const Color primary = const Color(0xFF481300);
-  static const Color background = const Color(0xFFE8E8E8);
-
-}
 
 class IconImage {
   static const String location =  "assets/icons/location.png";
